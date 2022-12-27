c9cb5a8b099b52f77f0b4ae3d14719e0c3000ae3
Michael Berry <michaelb@kentico.com>
2022-12-27 09:07:11 -0500

    Adds preview support

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['assets-us-01.kc-usercontent.com', 'di8m9w6rqrh5d.cloudfront.net'],
+    domains: ['assets-us-01.kc-usercontent.com', "preview-assets-us-01.kc-usercontent.com", 'di8m9w6rqrh5d.cloudfront.net'],
   },
 }
 
--- utilities/client.js
+++ utilities/client.js
@@ -3,7 +3,11 @@ import { createDeliveryClient } from '@kontent-ai/delivery-sdk';
 
 // initialize delivery client
 const deliveryClient = createDeliveryClient({
-    projectId: process.env.KONTENT_PROJECT_ID
+    projectId: process.env.KONTENT_PROJECT_ID,
+    previewApiKey: process.env.KONTENT_PREVIEW_KEY,
+    defaultQueryConfig: {
+        usePreviewMode: Boolean(process.env.KONTENT_PREVIEW)
+      }
 });
 
 export default deliveryClient
